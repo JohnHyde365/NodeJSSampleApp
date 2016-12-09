fb3d582fef8ceb9dadebf9755a7b4f4c230080dd
JohnBupaHyde365 <john.hyde@bupa.com>
2016-12-09 13:51:45 +0000

    added
    added

---
+++ JohnsExpressApp/test/AppTest.js
@@ -0,0 +1,13 @@
+﻿//simple async test for http 200 response code using super test
+'use strict';
+
+var request = require('supertest'), app = require('../app').app;
+
+describe('GET /', function () {
+    it('expects HTTP response 200', function (done) {
+        request(app)
+            .get('/')
+            .expect(200, done);
+    });
+});
+
